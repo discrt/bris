8705b97982390113462811d96b87343af685628b
GaziTaufiqIslam <gzidev@gmail.com>
2018-05-17 01:04:36 +0530

    FIrst code uploaded

---
+++ js/app.js
@@ -0,0 +1,46 @@
+ //Navbar functions
+    //Navabar responsive functions
+    $("#menu").on("click", function(){
+        console.log("hi");
+        $(".nav-items").toggleClass("slide");
+        $(".black-overlay").show();
+    });
+    $(".black-overlay, #close").on("click", function(){
+        $(".nav-items").toggleClass("slide");
+        $(".black-overlay").hide();
+    });
+
+    //--------Navabar responsive functions end
+
+   
+function navdocking(){
+    if ($(window).scrollTop()<=0) {
+        $("nav").addClass("nav-docked");
+    } else {
+        $("nav").removeClass("nav-docked");
+    }
+}
+navdocking();
+$(window).scroll(function(){
+    var Wscroll = $(window).scrollTop();
+    if (Wscroll>0) {
+        $("nav").removeClass("nav-docked");
+    } else {
+        $("nav").addClass("nav-docked");
+    }
+    console.log(Wscroll);
+});
+
+//----navabar function end
+
+//slideshow
+
+    $("#carousel").owlCarousel({
+    loop:true,
+    items:1,
+    autoWidth: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true
+
+    });
